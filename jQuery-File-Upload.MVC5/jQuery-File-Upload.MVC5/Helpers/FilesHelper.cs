59e8c0b4446d43e392b24c1777ec67f6c47a0d7a
JoeTseng <l4769153@gmail.com>
2018-04-20 16:28:35 +0800

    Fix delete thumb bug.

--- jQuery-File-Upload.MVC5/jQuery-File-Upload.MVC5/Helpers/FilesHelper.cs
+++ jQuery-File-Upload.MVC5/jQuery-File-Upload.MVC5/Helpers/FilesHelper.cs
@@ -58,7 +58,7 @@ namespace jQuery_File_Upload.MVC5.Helpers
             System.Diagnostics.Debug.WriteLine(System.IO.File.Exists(fullPath));
             String thumbPath = "/" + file + "80x80.jpg";
             String partThumb1 = Path.Combine(StorageRoot, "thumbs");
-            String partThumb2 = Path.Combine(partThumb1, file + "80x80.jpg");
+            String partThumb2 = Path.Combine(partThumb1, Path.GetFileNameWithoutExtension(file) + "80x80.jpg");
 
             System.Diagnostics.Debug.WriteLine(partThumb2);
             System.Diagnostics.Debug.WriteLine(System.IO.File.Exists(partThumb2));
